22e21e52aa0fbfd94203b5282c321fa7854a7781
qiaojie@xueleyun.com <88888888>
2016-07-07 23:01:42 +0800

    1

--- cloudroot/com-cloud-service/src/test/java/com/cloud/service/ServiceTest.java
+++ cloudroot/com-cloud-service/src/test/java/com/cloud/service/ServiceTest.java
@@ -1,14 +1,12 @@
 package com.cloud.service;
 
-import org.junit.Test;
-
 public class ServiceTest extends BaseTest{
 
-    @Test
-    public void testAddPraise() throws Exception {
-        String str = services.first();
-        System.out.println(str);
-    }
+//    @Test
+//    public void testAddPraise() throws Exception {
+//        String str = services.first();
+//        System.out.println(str);
+//    }
 
 
 }
